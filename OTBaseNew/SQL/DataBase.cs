f7c0e24723f39bfd438a7f75fd7612097a7dd072
Vitaliy Diduk <Tailer12012@gmail.com>
2017-01-20 12:20:53 +0200

    First commit

---
+++ OTBaseNew/SQL/DataBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+namespace OTBaseNew.SQL
+{
+    class DataBase
+    {
+        /// <summary>
+        /// Конект к БД
+        /// </summary>
+        private MySqlConnection connect = new MySqlConnection();
+        public DataBase()
+        {
+            //Строка конект к БД
+            string myConnectionString = "server=zlkmagaz.mysql.ukraine.com.ua;uid=zlkmagaz_zlkbase;pwd=zz2j45bq;database=zlkmagaz_zlkbase;";
+            connect.ConnectionString = myConnectionString;
+            connect.Open();
+        }
+        /// <summary>
+        /// Создание запроса к БД
+        /// </summary>
+        /// <param name="request">Строка запроса</param>
+        /// <returns>Результат запроса к БД</returns>
+        public MySqlDataReader MakeRequest(string request)
+        {
+            //Команда для создания запроса
+            MySqlCommand command = connect.CreateCommand();
+            //Присвоение запроса к команде
+            command.CommandText = request;
+            //Выполнение запроса
+            MySqlDataReader reader = command.ExecuteReader();
+            //Закрыть конект
+            connect.Close();
+            //Возврат результата
+            return reader;
+        }
+    }
+}
